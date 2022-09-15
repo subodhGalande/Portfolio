ae9d801f38c788872019c81edf98855fd2221e00
subodh Galande <subodh.dev@outlook.com>
2022-09-15 20:43:24 +0530

    popover design complete linking is left

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import App from "./../App";
-import { Popover } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Popover, Transition } from "@headlessui/react";
+import { ChevronDownIcon, XMarkIcon } from "@heroicons/react/20/solid";
 
 const Navbar = () => {
   return (
     <nav class=" container flex flex-1 mx-auto  items-baseline sm:justify-center justify-end mt-8 max-w-6xl px-4 sm:px-8 lg:px-12 ">
       <div class=" sm:hidden group text-zinc-300 text-sm font-medium justify-evenly py-2 px-3 w-auto rounded-full ring-1 ring-zinc-500/40  bg-zinc-800">
         <Popover className="relative">
-          <Popover.Button>
-            <span class="inline-block">Menu</span>
+          <Popover.Button class="outline-none">
+            <span class="inline-block ">Menu</span>
             <span>
               <ChevronDownIcon
                 className=" h-5 w-5 font-medium pl-1 text-zinc-300 inline-block "
@@ -18,17 +18,52 @@ const Navbar = () => {
               />
             </span>
           </Popover.Button>
+          <Popover.Overlay className="fixed z-5 inset-0 bg-black/80 opacity-100 backdrop-blur-sm  " />
 
-          <Popover.Panel className="absolute z-10">
-            <div className="grid grid-cols-2">
-              <a href="/analytics">Analytics</a>
-              <a href="/engagement">Engagement</a>
-              <a href="/security">Security</a>
-              <a href="/integrations">Integrations</a>
-            </div>
-
-            <img src="/solutions.jpg" alt="" />
-          </Popover.Panel>
+          <Transition
+            enter="transition-opacity duration-500"
+            enterFrom="opacity-0 ease-in "
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300"
+            leaveFrom="opacity-100  "
+            leaveTo="opacity-0"
+          >
+            <Popover.Panel className="absolute z-10 ">
+              <div class="fixed grid inset-x-4 top-7 z-50 origin-top rounded-3xl p-8 ring-1 ring-zinc-500/40 bg-zinc-900  opacity-100 scale-100">
+                {" "}
+                <div class="flex flex-row justify-between">
+                  <h6 class="inline-block text-zinc-400">Menu</h6>
+                  <Popover.Button
+                    class="inline-block"
+                    aria-label="Close menu"
+                    type="button"
+                    data-headlessui-state="open"
+                    tabindex="0"
+                  >
+                    {" "}
+                    <XMarkIcon
+                      className=" h-5 w-5 font-medium pl-1 text-zinc-400 inline-block "
+                      stroke="currentColor"
+                      stroke-width="1"
+                    />
+                  </Popover.Button>
+                </div>
+                <ul class=" mt-4 divide-y divide-zinc-100 font-sans text-base font-normal text-zinc-300 divide-zinc-100/5 ">
+                  <li>
+                    {" "}
+                    <Popover.Button class="pt-3 pb-2">About</Popover.Button>
+                  </li>
+                  <li>
+                    <Popover.Button class="pt-3 pb-2">Projects</Popover.Button>
+                  </li>
+                  <li>
+                    <Popover.Button class="pt-3 pb-2">Blog</Popover.Button>
+                  </li>
+                </ul>
+              </div>
+              <img src="/solutions.jpg" alt="" />
+            </Popover.Panel>
+          </Transition>
         </Popover>
       </div>
       <div>
@@ -50,3 +85,26 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+<button
+  aria-label="Close menu"
+  class="-m-1 p-1"
+  type="button"
+  data-headlessui-state="open"
+  tabindex="0"
+>
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    class="h-6 w-6 text-zinc-500 dark:text-zinc-400"
+  >
+    <path
+      d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</button>;
