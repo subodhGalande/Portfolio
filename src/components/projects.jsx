dd290983837ffaf58c0b6d63f87fbc36fdd5b4c3
subodh Galande <subodh.dev@outlook.com>
2022-10-09 15:13:28 +0530

    project page design check

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import one from "../assets/onedesk.png";
+import one from "../assets/onedesk1.png";
 import sbmo from "../assets/sbmo.png";
 import stox from "../assets/stoxticker.png";
 import { FiGithub } from "react-icons/fi";
@@ -9,42 +9,113 @@ const Projects = () => {
     <>
       <section className="container mx-auto mt-28 px-4 md:px-8 lg:px-12">
         {" "}
-        <p class=" text-center lg:text-5xl text-4xl tracking-normal font-pop font-bold text-zinc-100 max-w-4xl">
+        <p class=" text-center mx-auto lg:text-5xl text-4xl tracking-normal font-pop font-bold text-zinc-100 max-w-4xl">
           Things I've Built
         </p>{" "}
-        <div className=" relative mt-20 mx-auto h-[28rem] w-full  ">
-          <img
-            src={one}
-            className=" h-full object-cover mx-auto rounded-xl shadow-zinc-900 shadow-md brightness-[.1] "
-          />
-          <span className=" absolute top-0 px-6 ">
-            <div className="mt-10">
-              <small className="text-cyan-400 text-sm font-mono">
-                Featured Project
-              </small>
-              <h1 className="text-white text-2xl mt-2 font-semibold ">
-                OneDesk
-              </h1>
+        <div className=" mt-20 grid sm:grid-cols-4 lg:grid-cols-3 gap-6  ">
+          <div
+            style={{
+              background: `url(${one})`,
+              backgroundRepeat: "no-repeat",
+            }}
+            className=" sm:col-span-2 lg:col-span-1 bg-cover h-fit  w-fit rounded-xl overflow-hidden  "
+          >
+            <div className=" backdrop-brightness-[.08] opacity-100 py-1 px-6 ">
+              <div className="mt-10">
+                <small className="text-cyan-400 text-sm font-mono">
+                  Featured Project
+                </small>
+                <h1 className="text-white text-2xl mt-2 font-semibold ">
+                  OneDesk
+                </h1>
+              </div>
+
+              <h2 className="text-zinc-400 text-base mt-10 ">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos, laborum veniam earum unde rerum dolor sunt magnam
+                dolores aperiam dolore incidunt rem tenetur quam numquam quasi
+                nobis nam sint quo?{" "}
+              </h2>
+              <div className="w-full">
+                <ul className="mt-14 text-sm text-white font-mono flex flex-wrap space-x-4 ">
+                  <li className=" shrink-0">Bulma CSS</li>
+                  <li>Javascript</li>
+                  <li>PHP</li>
+                  <li>MySQL</li>
+                </ul>
+              </div>
+              <FiGithub class="text-white h-5 w-5 mt-6 mb-10 " />
             </div>
+          </div>
+          <div
+            style={{
+              background: `url(${stox})`,
+              backgroundRepeat: "no-repeat",
+            }}
+            className=" sm:col-span-2 lg:col-span-1  h-max bg-cover bg-top w-full rounded-xl overflow-hidden  "
+          >
+            <div className=" backdrop-brightness-[.08] opacity-100 py-1 px-6 ">
+              <div className="mt-10">
+                <small className="text-cyan-400 text-sm font-mono">
+                  Featured Project
+                </small>
+                <h1 className="text-white text-2xl mt-2 font-semibold ">
+                  StoxTicker
+                </h1>
+              </div>
 
-            <h2 className="text-zinc-300 text-base mt-10">
-              {" "}
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Dignissimos, laborum veniam earum unde rerum dolor sunt magnam
-              dolores aperiam dolore incidunt rem tenetur quam numquam quasi
-              nobis nam sint quo?{" "}
-            </h2>
+              <h2 className="text-zinc-400 text-base mt-10 ">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos, laborum veniam earum unde rerum dolor sunt magnam
+                dolores aperiam dolore incidunt rem tenetur quam numquam quasi
+                nobis nam sint quo?{" "}
+              </h2>
+              <div className="w-full">
+                <ul className="mt-14 text-sm text-white font-mono flex flex-wrap space-x-4 ">
+                  <li>React</li>
+                  <li>Ionic Framework</li>
+                  <li>Styled Components</li>
+                </ul>
+              </div>
+              <FiGithub class="text-white h-5 w-5 mt-6 mb-10 " />
+            </div>
+          </div>
+          <div
+            style={{
+              background: `url(${sbmo})`,
+              backgroundRepeat: "no-repeat",
+            }}
+            className=" sm:col-span-2 lg:col-span-1 sm:col-start-2 h-max bg-cover w-full rounded-xl overflow-hidden "
+          >
+            <div className=" backdrop-brightness-[.08] opacity-100 py-1 px-6 ">
+              <div className="mt-10">
+                <small className="text-cyan-400 text-sm font-mono">
+                  Featured Project
+                </small>
+                <h1 className="text-white text-2xl mt-2 font-semibold ">
+                  SBM Overseas
+                </h1>
+              </div>
 
-            <ul className="mt-14 text-sm text-white font-mono flex space-x-4 ">
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript</li>
-              <li>PHP</li>
-              <li>MySQL</li>
-            </ul>
-
-            <FiGithub class="text-white h-7 w-6 mt-6 mb-10 " />
-          </span>
+              <h2 className="text-zinc-400 text-base mt-10 ">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dignissimos, laborum veniam earum unde rerum dolor sunt magnam
+                dolores aperiam dolore incidunt rem tenetur quam numquam quasi
+                nobis nam sint quo?{" "}
+              </h2>
+              <div className="w-full">
+                <ul className="mt-14 text-sm text-white font-mono flex flex-wrap space-x-4 ">
+                  <li>React</li>
+                  <li>Tailwind CSS</li>
+                  <li>Vite</li>
+                </ul>
+              </div>
+              <FiGithub class="text-white h-5 w-5 mt-6 mb-10 " />
+            </div>
+          </div>
         </div>
       </section>
     </>
